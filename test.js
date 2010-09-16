ac38643d266a5760b5702240ec1be4a3a7cb542a
Caolan McMahon <caolan@caolanmcmahon.com>
2010-09-16 09:34:06 +0100

    fix paths in test script to avoid running the tests of a copy installed via npm

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 #!/usr/local/bin/node
 
-require.paths.push(__dirname);
-require.paths.push(__dirname + '/lib');
-var testrunner = require('nodeunit').testrunner;
+var testrunner = require('./lib/nodeunit').testrunner;
 
 process.chdir(__dirname);
 testrunner.run(['test']);
