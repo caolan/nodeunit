5811426cac8cf8c59413feb3bf039cdfcd375ce5
Richard Gibson <richard.gibson@gmail.com>
2013-01-03 14:08:26 -0500

    improve test: testNonAssertionError

--- test/test-bettererrors.js
+++ test/test-bettererrors.js
@@ -61,7 +61,7 @@ exports.testNonAssertionError = function (test) {
         throw new Error("test error");
     } catch (error) {
         var betterErrorString = betterErrorStringFromError(error);
-        betterErrorString.should.not.include("AssertionError");
+        betterErrorString.should.not.match(/\bAssertionError/);
         betterErrorString.should.include("Error");
         betterErrorString.should.include("test error");
         betterErrorString.should.include("test-bettererrors");
